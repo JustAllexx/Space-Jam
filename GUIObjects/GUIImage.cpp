9c06e74dde3fe3dfd0cf0cf18fb83f22563cd723
Alex Wilkinson <alex.wilkinson@student.manchester.ac.uk>
2026-08-04 15:35:12 +0100

    ImageGUI now is perfectly statically initialised without recalculating vertex data

--- GUIObjects/GUIImage.cpp
+++ GUIObjects/GUIImage.cpp
@@ -1,13 +1,26 @@
 #include "GUIImage.h"
 #include <stb/stb_image.h>
 
+std::array<std::array<float, 4>, 6> GUIImage::createVertexData(float x, float y, float size, int height, int width) {
+	float h = static_cast<float>(height) * size * 0.5f;
+	float w = static_cast<float>(width) * size * 0.5f;
+	return {{
+			{ x - w,     y + h,   0.0f, 0.0f },
+			{ x - w,     y - h,       0.0f, 1.0f },
+			{ x + w ,    y - h,       1.0f, 1.0f },
 
+			{ x - w,     y + h,   0.0f, 0.0f },
+			{ x + w,     y - h,       1.0f, 1.0f },
+			{ x + w,     y + h,   1.0f, 0.0f }
+	}};
+}
+
+//Texture flip needs to be false here
 GUIImage::GUIImage(const char* imagePath, float inX, float inY, float inScale, GLuint inVAO, GLuint inVBO, GLuint inGUIShader) 
-: GUIObject(inGUIShader), texture(imagePath, false) {
-	posX = inX; posY = inY; scale = inScale;
-	VAO = inVAO; VBO = inVBO;
-	imgWidth = texture.getImageWidth();
-	imgHeight = texture.getImageHeight();
+: GUIObject(inGUIShader), posX(inX),
+  posY(inY), scale(inScale), texture(imagePath, false),
+  imgHeight(texture.getImageHeight()), imgWidth(texture.getImageWidth()), VAO(inVAO), VBO(inVBO),
+  vertexInfo(createVertexData(posX, posY, scale, imgHeight, imgWidth)) {
 }
 
 //Render Function for ImagGUI class, an override for the GUIObject class, very similar to rendering a quad for a character, but calculates the coordinates of the quad slightly differently
@@ -19,22 +32,9 @@ void GUIImage::Render() {
 	glActiveTexture(GL_TEXTURE0);
 	glBindVertexArray(VAO);
 
-	float h = static_cast<float>(imgHeight) * scale * 0.5f;
-	float w = static_cast<float>(imgWidth) * scale * 0.5f;
-
-	float vertices[6][4] = {
-			{ posX - w,     posY + h,   0.0f, 0.0f },
-			{ posX - w,     posY - h,       0.0f, 1.0f },
-			{ posX + w , posY - h,       1.0f, 1.0f },
-
-			{ posX - w,     posY + h,   0.0f, 0.0f },
-			{ posX + w, posY - h,       1.0f, 1.0f },
-			{ posX + w, posY + h,   1.0f, 0.0f }
-	};
-
 	glBindTexture(GL_TEXTURE_2D, texture.getTextureID());
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertexInfo), vertexInfo.data());
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	// render quad
 	glDrawArrays(GL_TRIANGLES, 0, 6);
--- GUIObjects/GUIImage.h
+++ GUIObjects/GUIImage.h
@@ -2,15 +2,21 @@
 
 #include "GUIObject.h"
 #include "../ObjectLoader.h"
+#include <array>
 
 //The class for displaying Images, is a GUI element and so inherits from the GUI Element class
 class GUIImage : public GUIObject {
 private:
+	static std::array<std::array<float, 4>, 6> createVertexData(float x, float y, float size, int height, int width);
+
 	//Properties about the image, location ID of image texture
 	float posX, posY, scale;
-	int imgHeight, imgWidth;
 	Texture texture;
+	int imgHeight, imgWidth;
 	GLuint VAO, VBO;
+
+	//Vertex information for drawing
+	std::array<std::array<float, 4>, 6> vertexInfo;
 public:
 	//Constructor function for image class
 	GUIImage(const char* imagePath, float inX, float inY, float inScale, GLuint inVAO, GLuint inVBO, GLuint inGUIShader);
