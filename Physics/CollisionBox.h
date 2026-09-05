d27c069f6044da039eba65a1e454d577dd8022d6
Alex Wilkinson <alex.wilkinson@student.manchester.ac.uk>
2026-09-05 21:08:39 +0100

    Deleted unneccessary CMAKE files

--- Physics/CollisionBox.h
+++ Physics/CollisionBox.h
@@ -3,7 +3,7 @@
 //#include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
-#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include <vector>
